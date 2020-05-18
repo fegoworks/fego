21d653cb02a1ef916955cfcba1915b8f9a34533c
unknown <fegoworks@hotmail.com>
2020-05-18 21:11:08 +0100

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+/** @format */
+
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => {
+  return <h2>Testing</h2>;
+};
+
+render(<App />, document.querySelector('#root'));
