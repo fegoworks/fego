916e1c9799df61453052472bba620b3260f31bd1
unknown <fegoworks@hotmail.com>
2020-06-03 23:58:46 +0100

    feat(body): add new font family

--- src/index.jsx
+++ src/index.jsx
@@ -2,16 +2,10 @@
 
 import React from 'react';
 import { render } from 'react-dom';
-import Header from './components/commons/Header';
 import PageTemplate from './components/pages/home/PageTemplate';
 
 const App = () => {
-  return (
-    <React.Fragment>
-      <Header />
-      <PageTemplate />
-    </React.Fragment>
-  );
+  return <PageTemplate />;
 };
 
 render(<App />, document.querySelector('#root'));
