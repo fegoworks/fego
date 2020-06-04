00dae42c5befe2682302d2b56db8cbd4430d2a65
unknown <fegoworks@hotmail.com>
2020-06-04 23:55:28 +0100

    chore(featured-work): add portfolio link

---
+++ src/data/works.js
@@ -0,0 +1,38 @@
+/** @format */
+
+const image = [
+  {
+    work: 'portfolio',
+    url:
+      'https://res.cloudinary.com/fego/image/upload/v1591311087/fego/fego_qbn42g.jpg',
+    detail: `My portfolio page. Built with reactJs`,
+  },
+  {
+    work: 'teamwork',
+    url:
+      'https://res.cloudinary.com/fego/image/upload/v1590153226/fego/teamwork_ukmidc.png',
+    detail: `Teamwork is an ​internal social network for employees of an organization. The goal
+     of this application is to facilitate more interaction between colleagues and promote team bonding`,
+  },
+  {
+    work: 'chef-folio',
+    url:
+      'https://res.cloudinary.com/fego/image/upload/v1590155104/fego/chef-folio_nmzw13.jpg',
+    detail: `An application for chefs to show off their work and recipes. They could share with potential clients,
+     the amazing pics from the recipes they’ve created.`,
+  },
+  {
+    work: 'youtbsrch',
+    url:
+      'https://res.cloudinary.com/fego/image/upload/v1590156976/fego/youtbsrchjpg_atimgj.jpg',
+    detail: `A Youtube search app`,
+  },
+  {
+    work: 'quiz',
+    url:
+      'https://res.cloudinary.com/fego/image/upload/v1590243165/fego/quiz_gk1ekd.jpg',
+    detail: `A simple quiz app built with vanilla javascript`,
+  },
+];
+
+export default image;
